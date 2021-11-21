a4e56c6c0d69364eaa11960cf55dba82459c4ee9
Igor <n.i.v.laurente1@newcastle.ac.uk>
2021-11-21 00:22:52 +0000

    Finish implementing binarySearch method

--- search/binary.py
+++ search/binary.py
@@ -1,5 +1,29 @@
-def binarySearch(arry, searchValue):
-    return searchValue
+'''
+Methods to find a value using binary search algorithm
+Includes other methods such as insert and deleting values using binary search algorithm
+'''
+
+
+def binarySearch(arry, searchValue, L, R):
+    # If right pointer is less than left pointer then it's been recursively called to the point where all values have
+    # been compared
+    if R < L:
+        return f'{searchValue} was not found in {arry}'
+
+    # Find mid point in the array
+    m = int((L + R) / 2)
+
+    # Search value is the midpoint
+    if searchValue == arry[m]:
+        return f'{searchValue} was found in {arry}'
+    # When search value is greater than midpoint
+    if searchValue > arry[m]:
+        # Searches right half of the array
+        return binarySearch(arry, searchValue, m+1, R)
+    # When search value is less than midpoint
+    else:
+        # Searches left half of the array
+        return binarySearch(arry, searchValue, L, m-1)
 
 
 def binaryInsert(arry, newValue):
