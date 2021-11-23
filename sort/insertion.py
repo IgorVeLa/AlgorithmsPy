712ac22236419f13f26ca4be50978a210354c97e
Igor <n.i.v.laurente1@newcastle.ac.uk>
2021-11-23 23:29:06 +0000

    Implemented insertion sort method.

--- sort/insertion.py
+++ sort/insertion.py
@@ -1,6 +1,21 @@
-def insertionSort():
-    sortedArry = []
+'''
+Method to sort an array in ascending array using insertion sort algorithm
+'''
 
-    # TODO: implement insertion sort algorithm
 
-    return sortedArry
+def insertionSort(arry):
+    for i in range(len(arry)):
+        # Value that is needed to insert in correct place
+        key = arry[i]
+        # Current index to compare
+        j = i
+
+        # Compare to previous sorted elements
+        while j > 0 and key < arry[j-1]:
+            arry[j] = arry[j-1]
+            j = j-1
+
+        # Update key in correct position
+        arry[j] = key
+
+    return arry
